0916afd4a0923aa363cec0baca2af0e288682e52
Soloon Chamberrlin <solomon@no-reply.com>
2021-12-12 01:39:55 +0000

    Implemented boot xv6 style boot process

--- src/bootmain.rs
+++ src/bootmain.rs
@@ -8,15 +8,14 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-//static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
-pub extern "C" fn kernel_main() -> ! {
+pub extern "C" fn bootmain() -> ! {
     let buf = 0xb8000 as *mut u8;
     unsafe{
         *buf = 66;
         *buf.offset(1)=0xb;
     }
 
+    
     loop {}
 }
